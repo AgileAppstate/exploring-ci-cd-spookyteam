e0cec8f2e7f5694f0d610fd7888578aae3ee80cc
Michael-Mahn <MichaelMahn7@gmail.com>
2025-11-03 13:25:44 -0500

    added char counting script

---
+++ scripts/count_char.py
@@ -0,0 +1,10 @@
+
+from pathlib import Path
+
+file = Path("helloworld.py")
+content = file.read_text(encoding="utf-8")
+char_count = len(content)
+
+print(f"File: {file.name}")
+print(f"Characters: {char_count}")
+
